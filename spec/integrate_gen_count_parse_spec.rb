4e6d9e69ae51614d0485858a8f4970c6a3f18b21
edhowland <ed.howland@gmail.com>
2015-08-04 17:39:59 +0000

    \'added integrate_gen_count_parse_spec.rb\'

---
+++ spec/integrate_gen_count_parse_spec.rb
@@ -0,0 +1,20 @@
+# integrate_gen_count_parse_spec.rb - specs for integrate_gen_count_parse
+
+require_relative 'spec_helper'
+
+describe 'MdGen -> PageCounter -> TextParse' do
+  let(:gen) { MdGen.new }
+  let(:counter) { PageCounter.new }
+  let(:pipe) { RenderPipeline.new }
+  let(:runner) do
+    pipe << ->(x) { gen.eval_string x }
+      pipe << ->(x) { counter.process x }
+    pipe << ->(x) { text_parse_valve x }
+    pipe
+  end
+
+  describe 'page: h6, page: h6' do
+    subject { runner.run "page { h6 'Page [= page] of [= page_count] pages'}; page { }" }
+  end
+
+end
