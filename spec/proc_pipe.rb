0256774af1f495dda7b907bb9dbd70c2ce5ed59a
edhowland <ed.howland@gmail.com>
2015-08-22 21:52:31 +0000

    \'pipe method for Proc and called with :call_chain\'

---
+++ spec/proc_pipe.rb
@@ -0,0 +1,19 @@
+# proc_pipe.rb - method | on Proc
+
+
+class Proc
+  def |(prc)
+    @chain = prc
+    self
+  end  
+
+  attr_reader :chain
+
+  def call_chain value
+    unless @chain.nil?
+      @chain.call_chain(self.call(value)) 
+    else
+      self.call(value)
+  end
+  end
+end
