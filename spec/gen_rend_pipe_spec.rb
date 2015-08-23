a0cdc0740404acdc939ac2eb10e3187b61398744
edhowland <ed.howland@gmail.com>
2015-08-23 00:46:04 +0000

    \'got pipeline to work\'

--- spec/gen_rend_pipe_spec.rb
+++ spec/gen_rend_pipe_spec.rb
@@ -5,5 +5,9 @@ require_relative 'spec_helper'
 describe 'generate, then render' do
   let(:chain) {->(x) { MdGen.new.eval_string(x) } | ->(x) { GfmRender.new.render(x) } }
 
-  
+  describe 'h1 to # heading 1' do
+    subject { chain.call_chain("h1 'heading 1'") }
+
+    specify { subject.must_equal "# heading 1\n\n" }
+  end
 end
