0e32b1e7b9be56ae8b3410d7071d836f71eeabcb
edhowland <ed.howland@gmail.com>
2015-08-23 18:40:07 +0000

    \'chkpt page_pipe_spec.rb\'

--- spec/page_pipe_spec.rb
+++ spec/page_pipe_spec.rb
@@ -1,4 +1,4 @@
-# page_pipe_spec.rv - spec for running PageCounter in pipeline
+# page_pipe_spec.rv - spec for page counterpipeline
 
 require_relative 'spec_helper'
 
@@ -6,34 +6,35 @@ require_relative 'spec_helper'
 describe 'MdGen Pagination' do
   let(:gen) { MdGen.new }
   let(:counter) { PageCounter.new }
-  let(:pipe) { RenderPipeline.new }
-  let(:runner) do
-    p = pipe
-    p << ->(x) { gen.process &x }
-    p << ->(x) { counter.process x }
-    p
-  end
+  let(:chain) { ->(x){gen.eval_string(x) } | ->(x){ counter.process(x) } } 
+  #let(:pipe) { RenderPipeline.new }
+  #let(:runner) do
+    ##p = pipe
+    #p << ->(x) { gen.process &x }
+    #p << ->(x) { counter.process x }
+    #p
+  #end
 
 
   describe 'empty page break with no block' do
-    subject { runner.run { page{}  } }
+    subject { chain.call_chain " page{}  " }
 
     specify { subject.must_equal [[:page, 1, 1]]}
   end
   describe 'should have page method taking a block' do
-    subject { runner.run { page { h1 'text' } } }
+    subject { chain.call_chain { page { h1 'text' } } }
 
     specify {  subject.must_equal [[:h1, 'text'], [:page, 1, 1]] }
   end
 
   describe 'block with 2 page vlocks' do
-    subject { runner.run { page {}; page {} }}
+    subject { chain.call_chain { page {}; page {} }}
 
   specify {  subject.must_equal [[:page, 1, 2], [:page, 2,2]]  }
   end
 
   describe 'passing variables to page blocks' do
-    subject {runner.run  { page {|page, total| para "page: [= page], total: [= total]" } } }
+    subject { chain.call_chain  { page {|page, total| para "page: [= page], total: [= total]" } } }
 
   specify { subject.must_equal [[:para, [[:t, "page: "], [:equal, "page"], [:t, ", total: "], [:equal, "total"]]], [:page, 1, 1]]}
   end
