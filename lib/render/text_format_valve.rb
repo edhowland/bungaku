f573b4a2f0deae66c126d9a9ac5725f5f73dd55c
edhowland <ed.howland@gmail.com>
2015-08-26 00:27:30 +0000

    \'added text_format_valve.rb\'

---
+++ lib/render/text_format_valve.rb
@@ -0,0 +1,11 @@
+# text_format_valve.rb - method text_format_valve
+
+def is_heading? code
+  code[0] == :h1
+end
+
+
+# process code stream, formatting operands where needed
+def text_format_valve codes
+  codes
+end
