b39d4742ede4b1f857cc6192ca35e41cb68448fb
edhowland <ed.howland@gmail.com>
2015-08-26 16:07:48 +0000

    \'got link formatting working\'

--- lib/render/text_format_valve.rb
+++ lib/render/text_format_valve.rb
@@ -7,5 +7,6 @@ end
 # process code stream, formatting operands where needed
 def text_format_valve codes
   fmtr = TextFormat.new
-  codes.map_select(->(x){ is_heading?(x)}) {|e| e[1] = fmtr.format(e[1]); e }
+  codes.map_select(->(x){ is_heading?(x)}) {|e| e[1] = fmtr.format(e[1]); e 
+  }.map_select(->(x){ x[0] == :a})  {|e|name = e[1][0]; link = e[1][1]; name =  fmtr.format(name); [:a, [name, link]] }
 end
--- spec/text_format_valve_spec.rb
+++ spec/text_format_valve_spec.rb
@@ -23,4 +23,13 @@ describe 'text_format_valve' do
 
     specify { subject.must_equal [[:h1, 'this is **heading**']] }
   end
+
+  describe 'link' do
+    let(:codes) { [[:a, ['go to [ital example.com]', 'http://example.com']] ] }
+    let(:lparse) { ->(x){ text_parse_valve(x) } }
+    let(:chain) { lparse | ->(x){ text_format_valve(x) }; lparse }
+    subject { chain.call_chain codes }
+
+  specify { subject.must_equal [[:a, ['go to *example.com*', 'http://example.com']]] }
+  end
 end
