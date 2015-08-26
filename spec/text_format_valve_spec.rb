99117d918e163c80fa6c4bcc10d768605e2f0891
edhowland <ed.howland@gmail.com>
2015-08-26 00:26:48 +0000

    \'is_heading? working for h1\'

--- spec/text_format_valve_spec.rb
+++ spec/text_format_valve_spec.rb
@@ -1,3 +1,22 @@
 # text_format_valve_spec.rb - specs for text_format_valve
 
 require_relative 'spec_helper'
+
+describe 'text_format_valve' do
+  describe 'empty' do
+    specify { text_format_valve([]).must_equal [] }
+  end
+
+  describe 'is_heading?' do
+    specify { is_heading?([:h1, []]).must_equal true }
+  end
+
+  describe 'h1 "bold [bold heading]"' do
+    let(:codes) { [[:h1, 'this is  [bold heading]']] }
+    let(:lparse) { ->(x){ text_parse_valve(x) } }
+    let(:chain) { lparse | ->(x){ text_format_valve(x) }; lparse }
+    subject { chain.call_chain codes }
+
+    specify { subject.must_equal [[:h1, 'this is *heading']] }
+  end
+end
